eab61fd6c411f78883ef5bb26d7ac4eb02a59300
MaddynKatie <probncentermaddy@gmail.com>
2026-05-06 15:00:47 -0400

    upd

---
+++ assets/games-BMmdm68D.js
@@ -0,0 +1,1 @@
+var e=[{id:`demo`,name:`Demo`,icon:`./assets/icons/demo.png`},{id:`bvbaseplate`,name:`Blockverse Baseplate`,icon:`./assets/icons/demo.png`,mapPath:`./assets/models/bvBaseplate.json`},{id:`classichouse`,name:`Classic House`,icon:`./assets/icons/classichouse.png`,mapPath:`./assets/models/classichouse.json`,spawn:{z:4}}];export{e as t};
